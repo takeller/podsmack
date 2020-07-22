65a514deb2c91a2bffa0f16c71986fb9c9a44943
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-22 16:24:08 -0600

    add password validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates :password_digest, presence: true
+  validates_confirmation_of :password
+  has_secure_password
   validates :user_name, presence: true
   validates :user_name, uniqueness: true, presence: true
   has_many :followings
