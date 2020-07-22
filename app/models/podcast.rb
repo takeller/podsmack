382ca37e8e4426eb8d4d0d093c18fb5078f9a5c1
Jane Greene <janegreenecasa@gmail.com>
2020-07-22 14:49:40 -0600

    passing model tests

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,3 +1,5 @@
 class Podcast < ApplicationRecord
   belongs_to :producer
+  has_many :podcast_tags, dependent: :destroy
+  has_many :tags, through: :podcast_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :password_digest, presence: true
+  validates :user_name, presence: true
+  has_many :followings
+  has_many :podcasts, through: :followings
 end
