725ab07f29befd7df12365baba943efc78d6fb1e
Jane Greene <janegreenecasa@gmail.com>
2020-07-22 14:38:28 -0600

    Created model tests

--- spec/models/podcast_tag_spec.rb
+++ spec/models/podcast_tag_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PodcastTag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to :podcast}
+  it {should belong_to :tag}
 end
