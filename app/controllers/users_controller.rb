0f5077105ef50bfdb75dffa6e06ca187d06af308
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-22 16:24:25 -0600

    add create user controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = 'Successfully registered and logged in'
+      redirect_to dashboard_path
+    else
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
