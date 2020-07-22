59d18ac31f379b37292bcfe5a80a213317f0cfa5
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-22 16:13:04 -0600

    add new user controller action and view

--- spec/features/visitors/visitor_can_register_spec.rb
+++ spec/features/visitors/visitor_can_register_spec.rb
@@ -2,15 +2,11 @@ require 'rails_helper'
 
 describe 'visitor can create an account' do
   it 'can create account from registration page' do
-    name = 'user1'
-    password = 'password'
-    password_confirmation = 'password'
-
     visit new_user_path
 
-    fill_in :name,  with: name
-    fill_in :password, with: password
-    fill_in :password_confirmation, with: password
+    fill_in 'user[user_name]',  with: 'user1'
+    fill_in 'user[password]', with: 'password'
+    fill_in 'user[password_confirmation]', with: 'password'
     click_button "Submit"
 
     expect(current_path).to eq(profile_path)
