2a3d85a09314e1fd9a67aa8df4782004e11d76ea
Taylor Keller <takeller17@gmail.com>
2020-07-22 16:55:39 -0600

    Adds tags column to applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_204907) do
+ActiveRecord::Schema.define(version: 2020_07_22_225457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_204907) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
+    t.string "tags"
     t.index ["user_id"], name: "index_applications_on_user_id"
   end
 
