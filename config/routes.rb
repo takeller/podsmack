524b35fee4ae11074d98d9a81a8339c6392418ba
Taylor Keller <takeller17@gmail.com>
2020-07-23 14:46:48 -0600

    In progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failutre', to:redirect('/')
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
+  get '/auth/failure', to:redirect('/')
+  get '/login', to: 'sessions#new'
 
   root 'welcome#index'
   resources :users, only: [:new, :create] do
     resources :applications, only: [:new, :create]
-  end 
+  end
   get '/dashboard', to: 'users#show'
 
   resources :producers, only: [:show]
