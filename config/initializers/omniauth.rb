3502520dd0e7af602a7f136e222ce3796e63e804
Taylor Keller <takeller17@gmail.com>
2020-07-23 17:44:38 -0600

    Adds omniauth routes and initializer for google provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-    scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online'
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
