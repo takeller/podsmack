a15cfbaa70e0d27015c4b8a9f43ab5dd57362a52
Taylor Keller <takeller17@gmail.com>
2020-07-23 13:13:25 -0600

    Adds default value of unapproved to application status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_225457) do
+ActiveRecord::Schema.define(version: 2020_07_23_191204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_225457) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "status"
+    t.string "status", default: "unapproved"
     t.string "tags"
     t.index ["user_id"], name: "index_applications_on_user_id"
   end
