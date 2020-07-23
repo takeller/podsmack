1ae23a839f90a2a8d04603d5b83afae2ed704dfc
Taylor Keller <takeller17@gmail.com>
2020-07-23 14:04:34 -0600

    Adds omniauth gem and initializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failutre', to:redirect('/')
+  
   resources :users, only: [:new, :create]
   get '/dashboard', to: 'users#show'
 
