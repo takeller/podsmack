fc0cb24770ce895aec86be390a5dd917050e4ce3
Taylor Keller <takeller17@gmail.com>
2020-07-22 18:46:09 -0600

    Form works and sends correct params to controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,4 +3,23 @@ class ApplicationsController < ApplicationController
   def new
     @application = Application.new
   end
+
+  def create
+    user = User.find(params[:user_id])
+    application = user.applications.new(application_params)
+
+    if application.save
+      flash[:notice] = 'Application Submitted'
+      redirect_to '/dashboard'
+    else
+      flash[:notice] = 'Submission Failed'
+      render :new
+    end
+  end
+
+  private
+
+  def application_params
+    params.require(:application).permit(:podcast_name, :location, :description, :patreon, :instagram, :facebook, :twitter, :spotify_uri, :photo, :adult_content, :tags => [])
+  end
 end
--- spec/features/users/podcast_application_spec.rb
+++ spec/features/users/podcast_application_spec.rb
@@ -4,35 +4,41 @@ describe 'As a registered user' do
   describe 'When I visit my dashboard' do
     it 'I can apply to submit a podcast' do
       user = create(:user)
+      create(:tag, name: 'Interviews')
+      create(:tag, name: 'Music')
+      create(:tag, name: 'Software')
+      #
+      # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      #
+      # visit '/dashboard'
+      #
+      # click_on 'Podcast Application'
+      #
+      # expect(current_path).to eq('/application/new')
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit new_user_application_path(user)
 
-      visit '/dashboard'
+      fill_in 'application[podcast_name]', with: user.user_name
+      page.select 'Denver', from: 'application[location]'
+      check 'Interviews'
+      check 'Music'
+      check 'Software'
+      fill_in 'application[twitter]', with: 'www.twitter.com/test'
+      fill_in 'application[patreon]', with: 'www.patreon.com/test'
+      fill_in 'application[instagram]', with: 'www.instagram.com/test'
+      fill_in 'application[facebook]', with: 'www.facebook.com/test'
+      fill_in 'application[description]', with: 'Favorite albums broken down'
+      check 'application[adult_content]'
+      fill_in 'application[spotify_uri]', with: '2b025hq3gJ17tQdxS3aV43'
+      fill_in 'application[photo]', with: 'https://picsum.photos/200'
 
-      click_on 'Podcast Application'
-
-      expect(current_path).to eq('/application/new')
-
-      fill_in 'name', with: user.name
-      # Select from dropdown
-      fill_in 'location', with: 'Denver'
-      fill_in 'twitter', with: 'www.twitter.com/test'
-      fill_in 'patreon', with: 'www.patreon.com/test'
-      fill_in 'instagram', with: 'www.instagram.com/test'
-      fill_in 'facebook', with: 'www.facebook.com/test'
-      fill_in 'description', with: 'Favorite albums broken down'
-      # Checkbox
-      fill_in 'adult', with: 'true'
-      fill_in 'spotify', with: '2b025hq3gJ17tQdxS3aV43'
-      fill_in 'photo', with: 'https://picsum.photos/200'
-
-      click_in 'Submit'
+      click_on 'Submit'
 
       expect(current_path).to eq('/dashboard')
 
       application = Application.last
 
-      expect(application.name).to eq(user.name)
+      expect(application.name).to eq(user.user_name)
       expect(application.location).to eq('Denver')
       expect(application.show_id).to eq('2b025hq3gJ17tQdxS3aV43')
     end
