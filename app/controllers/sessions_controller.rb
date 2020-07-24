9493a43418f0cc6d58f39cb2d4382ea4e457dba4
Jane Greene <janegreenecasa@gmail.com>
2020-07-23 19:02:04 -0600

    Edited user schema to allow for creation from google oauth WIP

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 class SessionsController < ApplicationController
 
   def new
+
   end
 
   def create
-    binding.pry
+    user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:user_id] = user.id
+    flash[:success] = "Welcome #{user.user_name}"
+
+    redirect_to '/dashboard'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
-  validates_presence_of :password, require: true, on: :create
-  validates_confirmation_of :password
-  has_secure_password
+  # validates_presence_of :password, require: true, on: :create
+  # validates_confirmation_of :password
+  # has_secure_password
   validates :user_name, presence: true
   validates :user_name, uniqueness: true, presence: true
   has_many :followings
   has_many :podcasts, through: :followings
   has_many :applications
+
+  def self.from_omniauth(params)
+    user = find_or_create_by(uid: params[:uid])
+    user.user_name = params[:info][:name]
+    user.email = params[:info][:email]
+    user.save!
+    user
+  end
 end
