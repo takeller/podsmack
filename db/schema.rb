650a5e89d81648044b63abed413cd5d42dfc718d
Taylor Keller <takeller17@gmail.com>
2020-07-23 18:18:48 -0600

    Adds email to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_191204) do
+ActiveRecord::Schema.define(version: 2020_07_24_001825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2020_07_23_191204) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "applications", "users"
