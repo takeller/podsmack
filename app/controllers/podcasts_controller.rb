536be64d21cb68b4802f2e59bb3ad0158f324566
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-23 18:05:25 -0600

    change applications to podcasts

---
+++ app/controllers/podcasts_controller.rb
@@ -0,0 +1,26 @@
+class PodcastsController < ApplicationController
+
+  def new
+    @podcast = Podcast.new
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    binding.pry
+    podcast = user.podcasts.new(podcast_params)
+
+    if podcast.save
+      flash[:notice] = 'Podcast submitted and waiting approval'
+      redirect_to '/dashboard'
+    else
+      flash[:error] = podcast.errors.full_messages.to_sentence
+      redirect_to new_user_podcast_path(user)
+    end
+  end
+
+  private
+
+  def podcast_params
+    params.require(:podcast).permit(:name, :location, :description, :patreon, :instagram, :facebook, :twitter, :spotify_uri, :photo, :adult_content, :tags => [])
+  end
+end
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -2,4 +2,7 @@ class Podcast < ApplicationRecord
   belongs_to :user
   has_many :podcast_tags, dependent: :destroy
   has_many :tags, through: :podcast_tags
+  validates_presence_of :name
+  validates_presence_of :location 
+  validates_presence_of :description
 end
