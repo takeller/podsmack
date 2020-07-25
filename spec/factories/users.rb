64eec48bac919a9a330231879923305c3dd6dd97
Taylor Keller <takeller17@gmail.com>
2020-07-25 09:21:15 -0600

    Adds uid and removes password_digest from users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :user do
     user_name { Faker::Artist.name }
+    uid { rand(100000).to_s }
     access_token { Faker::Crypto.md5 }
     refresh_token { Faker::Crypto.md5 }
     admin { :default  }
-    password_digest { Faker::Creature::Dog.name }
   end
 
   factory :admin, parent: :user do
