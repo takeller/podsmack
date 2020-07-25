bc8def3982ef7d9c3f1064cb1d2616c32dc9a03a
Danny Ramos <danramos21@gmail.com>
2020-07-25 17:50:13 -0600

    approval spec and view

--- spec/features/admins/admin_dashboard_spec.rb
+++ spec/features/admins/admin_dashboard_spec.rb
@@ -1,22 +1,39 @@
 require 'rails_helper'
 
 describe 'As an Admin' do
-  it 'I visit the admin dashboard and see admin dashboard attributes' do
-    admin = create(:admin)
-    podcast = create(:podcast, user: admin)
-    podcast2 = create(:podcast, user: admin)
-    podcast3 = create(:podcast, user: admin)
-
-    login_user(admin)
+  before :each do
+    @admin = create(:admin)
+    @podcast = create(:podcast, user: @admin)
+    @podcast2 = create(:podcast, user: @admin)
+    @podcast3 = create(:podcast, user: @admin)
+    login_user(@admin)
 
     visit '/'
     click_on 'Login/Register'
 
     click_on 'Login With Google'
 
+  end
+  it 'I visit the admin dashboard and see admin dashboard attributes' do
+
     expect(current_path).to eq('/dashboard')
     expect(page).to have_css('.admin-dashboard')
-    # Podcasts pending approval
     expect(page).to have_css("#podcast_pending_approval", count: 3)
   end
+
+  it "An admin can approve pending podcasts" do 
+
+    within(first("#podcast_pending_approval")) do
+      expect(page).to have_button("Approve")
+      expect(page).to have_content(@podcast.name)
+      expect(page).to have_content(@podcast.description)
+    end
+
+    click_on("Approve")
+
+    expect(page).to have_content("Application approved")
+    expect(current_path).to eq('/dashboard')
+    
+  end
+
 end
