43c680d5fe7643dfb0544b9701b4c220fb8adcd8
Taylor Keller <takeller17@gmail.com>
2020-07-25 09:18:50 -0600

    Removes password digest from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_005110) do
+ActiveRecord::Schema.define(version: 2020_07_25_151758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(version: 2020_07_24_005110) do
     t.string "access_token"
     t.string "refresh_token"
     t.boolean "admin", default: false
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
