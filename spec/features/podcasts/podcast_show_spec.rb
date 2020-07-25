e722002b1433d4d7c063f434035cceeff212b1ee
Taylor Keller <takeller17@gmail.com>
2020-07-25 16:25:39 -0600

    Adds current_user to spec

--- spec/features/podcasts/podcast_show_spec.rb
+++ spec/features/podcasts/podcast_show_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 describe 'As a visitor' do
   describe 'When I visit a podcasts show page' do
     it 'I can see details about the podcast' do
+      user = create(:user)
       podcast = create(:podcast)
       bad_tags = create_list(:tag, 3)
       tags = create_list(:tag, 5)
       tags.each do |tag|
         create(:podcast_tag, podcast_id: podcast.id, tag_id: tag.id)
       end
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       visit "/podcasts/#{podcast.id}"
 
       expect(podcast.tags.count).to eq(5)
@@ -35,8 +38,9 @@ describe 'As a visitor' do
     end
 
     it 'I dont see a button to follow the podcast' do
+      user = create(:user)
       podcast = create(:podcast)
-
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       visit "/podcasts/#{podcast.id}"
 
       expect(page).to_not have_button('Follow')
@@ -47,7 +51,10 @@ end
 describe 'As a user' do
   describe 'When I visit a podcasts show page' do
     it 'I see a button to follow the podcast' do
+      user = create(:user)
       podcast = create(:podcast)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       visit "/podcasts/#{podcast.id}"
 
       expect(page).to have_button('Follow')
