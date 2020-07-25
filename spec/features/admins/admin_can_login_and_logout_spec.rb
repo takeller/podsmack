c85c60e8a6315fccf9dc4475fd8d4d9a6226d6f7
Jane Greene <janegreenecasa@gmail.com>
2020-07-25 14:10:22 -0600

    Added css to users show to present for admin only, corrected expectations for login tests

--- spec/features/admins/admin_can_login_and_logout_spec.rb
+++ spec/features/admins/admin_can_login_and_logout_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'As an Admin' do
-  xit 'I can login through google oauth' do
+  it 'I can login through google oauth' do
     admin = create(:admin)
 
     OmniAuth.config.test_mode = true
@@ -15,17 +15,27 @@ describe 'As an Admin' do
     visit '/'
     click_on 'Login/Register'
 
-    click_on '/Login With Google'
+    click_on 'Login With Google'
 
     expect(current_path).to eq('/dashboard')
     expect(page).to have_css('.admin-dashboard')
   end
 
-  xit 'I can logout' do
+  it 'I can logout' do
     admin = create(:admin)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      :provider => 'google_oauth2',
+      :uid => admin.uid,
+      :info => { :name => admin.user_name, email: "takeller17@gmail.com" },
+      :credentials => { :token => ENV['GOOGLE_API_TOKEN_TAYLOR'] }
+      })
 
-    visit '/dashboard'
+    visit '/'
+    click_on 'Login/Register'
+    click_on 'Login With Google'
+
+    expect(current_path).to eq('/dashboard')
 
     click_on 'Logout'
     expect(current_path).to eq(root_path)
