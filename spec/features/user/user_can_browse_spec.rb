1ce578027cafea9dce7769ff64327ac74169cbb9
Danny Ramos <danramos21@gmail.com>
2020-07-25 19:53:42 -0600

    User can browse spec w/ location

--- spec/features/user/user_can_browse_spec.rb
+++ spec/features/user/user_can_browse_spec.rb
@@ -4,27 +4,36 @@ describe 'As a User' do
   before :each do
     @user = create(:user)
     @podcast = create(:podcast, user: @user, location: "Denver")
-    @podcast2 = create(:podcast, user: @user, location: "New York")
+    @podcast2 = create(:podcast, user: @user, location: "NYC")
     @podcast3 = create(:podcast, user: @user, location: "Denver")
   
-    create(:tag, name: 'Interviews')
-    create(:tag, name: 'Music')
-    create(:tag, name: 'Software')
-    create(:tag, name: 'Mystery')
-    create(:tag, name: 'Sports')
+    @tag1 = create(:tag, name: 'Interviews')
+    @tag2 = create(:tag, name: 'Music')
+    @tag3 = create(:tag, name: 'Software')
+    @tag4 = create(:tag, name: 'Mystery')
+    @tag5 = create(:tag, name: 'Sports')
 
-    login_user
+    create(:podcast_tag, podcast_id: @podcast.id, tag_id: @tag1.id)
+    create(:podcast_tag, podcast_id: @podcast2.id, tag_id: @tag2.id) 
+    create(:podcast_tag, podcast_id: @podcast2.id, tag_id: @tag4.id)
+    create(:podcast_tag, podcast_id: @podcast3.id, tag_id: @tag3.id)
+    create(:podcast_tag, podcast_id: @podcast3.id, tag_id: @tag4.id)
+    create(:podcast_tag, podcast_id: @podcast3.id, tag_id: @tag5.id)
+    
+    login_user(@user) 
 
     visit '/'
-    click_on 'Login/Register'
-
-    click_on 'Login With Google'
+    click_on 'Browse'
 
   end
-  it 'I visit the admin dashboard and see admin dashboard attributes' do
+  it 'I find podcasts by location' do
 
-    expect(current_path).to eq('/dashboard')
-    expect(page).to have_css('.admin-dashboard')
-    expect(page).to have_css("#podcast_pending_approval", count: 3)
+    expect(current_path).to eq('/browse')
+    page.select 'Denver', from: 'podcast[location]'
+
+    click_on('Search')
+
+    expect(page).to have_css('.podcast-browse')
+    expect(page).to have_css("#podcast_results", count: 2)
   end
 end
