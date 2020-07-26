260df82864b5bddd687e1bb4a84fb18724279efe
Jane Greene <janegreenecasa@gmail.com>
2020-07-26 16:25:15 -0600

    Created a test and edited view so that dashboard link only appears for registers users

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -2,17 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "Podsmack Home Page" do
   describe "General introduction" do
-    it "Shows homepage " do
+    it "Shows homepage for unregistered guest" do
 
       visit '/'
 
       expect(page).to have_content('Podsmack')
       expect(page).to have_css("img[src*='giphy']")
       expect(page).to have_link("Podsmack")
-      expect(page).to have_link("Dashboard")
+      expect(page).to_not have_link("Dashboard")
       expect(page).to have_link("Browse")
       expect(page).to have_link("Login")
       expect(page).to have_link("Register")
+    end
+    it "Shows dashboard link once loged in" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit '/'
+      expect(page).to have_link("Dashboard")
     end
 
   end
