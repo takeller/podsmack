0fb89ab0081204259cab0c9e933fbe812d53d934
Jane Greene <janegreenecasa@gmail.com>
2020-07-26 13:58:47 -0600

    added test for episodes appearing on podcast show page

---
+++ spec/features/podcasts/episode_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "When I visit the podcast show page" do
+  describe "I see a list of podcast episodes " do
+
+    it "see episodes name" do
+        podcast = create(:podcast)
+        visit "/podcasts/#{podcast.id}"
+        
+        within(first('.episodes')) do
+          name = find(".name").text
+           expect(name).not_to be_empty
+        end
+    end
+    it "see episodes description" do
+
+    end
+    it "I can play a full episode" do
+
+    end
+  end
+
+end
