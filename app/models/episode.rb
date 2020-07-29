fed35a011854145d05b3ed1c5afdced23588f947
Taylor Keller <takeller17@gmail.com>
2020-07-28 19:01:06 -0600

    Updates show page methods to reflect changes in api

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,28 +1,31 @@
 class Episode
-  attr_reader :name, :description, :audio
+  attr_reader :title, :description, :audio, :audio_length_sec, :pub_date
 
   def initialize(formatted_episodes)
-    @name = formatted_episodes[:name]
+    @title = formatted_episodes[:title]
     @description = formatted_episodes[:description]
     @audio = formatted_episodes[:audio]
+    @audio_length_sec = formatted_episodes[:audio_length_sec]
+    @pub_date = formatted_episodes[:pub_date]
   end
 
   def self.make_episodes(spotify_uri)
       podcast_service = PodcastService.new
-      episode_details = podcast_service.episodes(spotify_uri)
-      format_episode_details(episode_details)
+      podcast_details = podcast_service.podcast(spotify_uri)
+      format_episode_details(podcast_details[:data][:episodes])
   end
 
 
   private
   def self.format_episode_details(episode_details)
-    episode_details[:episodes][:items][0..4].map do |details|
+    episode_details.map do |details|
       {
-        :name => details[:name],
+        :title => details[:title],
         :description => details[:description],
-        :audio => details[:audio_preview_url]
+        :audio => details[:audio],
+        :audio_length_sec => details[:audio_length_sec],
+        :pub_date => details[:pub_date]
       }
     end
-
   end
 end
