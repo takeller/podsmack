f49c30241be97240fa744df7e47ddc2d1086f677
Danny Ramos <danramos21@gmail.com>
2020-07-29 20:46:18 -0600

    fix database migration

--- db/migrate/20200729201621_remove_spotify_uri_from_podcasts.rb
+++ db/migrate/20200729201621_remove_spotify_uri_from_podcasts.rb
@@ -1,5 +1,5 @@
 class RemoveSpotifyUriFromPodcasts < ActiveRecord::Migration[6.0]
   def change
-    remove_column :podcasts, :podcast_uri, :string
+    remove_column :podcasts, :spotify_uri, :string
   end
 end
