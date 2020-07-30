fa15d031275a0710e6f973f2951927aa277d5297
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-29 19:01:48 -0600

    add from_api method and test for SearchResult

---
+++ app/facades/search_result.rb
@@ -0,0 +1,17 @@
+class SearchResult
+  attr_reader :title, :description, :thumbnail, :podcast_id
+
+  def initialize(result_params)
+    @title = result_params[:title]
+    @description = result_params[:description]
+    @thumbnail = result_params[:thumbnail]
+    @podcast_id = result_params[:id]
+  end
+
+  def self.from_api(search_term)
+    results = PodcastService.new.search(search_term)
+    results[:data][:results].map do |result|
+      SearchResult.new(result)
+    end
+  end
+end
---
+++ spec/models/search_result_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe SearchResult do
+ 
+  describe 'methods' do
+    it 'from_api ' do 
+      results = SearchResult.from_api('conan')
+      expect(results.length).to eq(10)
+      expect(results.first).to be_an_instance_of(SearchResult)
+      expect(results.last).to be_an_instance_of(SearchResult)
+      expect(results.first.title).to eq('Conan O’Brien Needs A Friend')
+    end
+  end
+end
